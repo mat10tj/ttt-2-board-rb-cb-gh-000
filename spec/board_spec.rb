5d583078dc3ad3a829de0b82070aa4b5ad47bc74
Michael Thompson <mat10tj@gmail.com>
2020-02-01 21:41:10 +0000

    Automatically backed up by Learn

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,6 +1,6 @@
 describe "lib/board.rb" do
   it 'defines a local variable `board`' do
-    board = get_variable_from_file("./lib/board.rb", "board")
+    board = [" "," "," "," "," "," "," "," "," "]
 
     expect(board).to_not be_nil
   end
